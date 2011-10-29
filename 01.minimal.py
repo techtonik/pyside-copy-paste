c5c918e8e36b49b2d54fab2689dc77c0210b145a
anatoly techtonik <techtonik@gmail.com>
2011-10-29 18:35:20 +0200

    + 01.minimal.py - a totally useless PySide application

---
+++ 01.minimal.py
@@ -0,0 +1,8 @@
+# The code is placed into public domain by anatoly techtonik
+# Feel free to copy/paste wherever you like
+
+# This is absolutely minimal example of PySide application
+from PySide.QtGui import QApplication
+
+QApplication([])
+ 
