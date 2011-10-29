f1af5c1711ea541c2d3aae5bfa8627640fa09371
anatoly techtonik <techtonik@gmail.com>
2011-10-29 19:30:05 +0200

    02.window.py - clean up comments

--- 02.window.py
+++ 02.window.py
@@ -1,22 +1,14 @@
 # The code is placed into public domain by anatoly techtonik
 # Feel free to copy/paste wherever you like
 
-# This is absolutely minimal example of PySide application
+# Absolutely minimal example of PySide application with window
+
 from PySide.QtGui import QApplication
 
-# Here an application object is created. It is the main
-# entrypoint through which you control underlying Qt framework.
+# Get entrypoint through which we control underlying Qt framework
 app = QApplication([])
-# Framework is quite powerful and contains a lot of stuff that
-# is useful in languages like C++, but in Python there are usually
-# better alternatives.
 
-# For example, empty list parameter [] to QApplication is a list
-# of command line arguments if you want to parse them with Qt,
-# but in Python apps it is usually done with optparse.
-
-# The next line starts the Qt/PySide application, which then waits
-# for events to process. But we haven't define any windows to
-# catch those events, so the app will just hang if we uncomment it
+# Start Qt/PySide application. If we don't define any windows, the
+# app would just hang at this point
 # app.exec_()
 
