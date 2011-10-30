e01434e25dd24adbacee4c5a24dbe575ff66bc60
anatoly techtonik <techtonik@gmail.com>
2011-10-30 09:53:51 +0200

    04.dialog.color.py: rough up comments

--- 04.dialog.color.py
+++ 04.dialog.color.py
@@ -1,24 +1,17 @@
 # The code is placed into public domain by anatoly techtonik
 # Feel free to copy/paste wherever you like
 
-# Absolutely minimal example of PySide application with button
+# Absolutely minimal example of PySide application with button calling dialog
 
 from PySide.QtGui import QApplication, QPushButton
 
 app = QApplication([])
-
-# Create button (which is also top level window)
+    
+# Create top level window/button
 button = QPushButton('Exit')
-# app.exit() is a method called to terminate application for
-# whatever reason you like. Here we instruct to call this method
-# when the button is clicked
+# call app.exit() when the button is clicked ('clicked' signal fires)
 button.clicked.connect(app.exit)
-# 'clicked' is a _signal_, which we _connect_ to a method that we
-# want to be called when signal fires
 button.show()
-
-# IMPORTANT: remember to assign GUI elements to variables or they
-# will be destroyed by PySide automatically
 
 app.exec_()
 
